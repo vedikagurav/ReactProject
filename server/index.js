11008c253dc9572ea290f31edc1e253a76d74392
vedikagurav <129783178+vedikagurav@users.noreply.github.com>
2025-05-19 14:48:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true     
+  }
+));
 app.use(express.json());
 
 // // app.use(cors(
